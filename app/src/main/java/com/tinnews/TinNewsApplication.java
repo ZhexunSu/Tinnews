49e79c9ecb49e87d40eacda211ac17228f121e78
ZhexunSu <zhexuns@uci.edu>
2020-05-27 21:26:47 -0700

    create network layer with Retrofit and its underlying OkHttp

---
+++ app/src/main/java/com/tinnews/TinNewsApplication.java
@@ -0,0 +1,15 @@
+package com.tinnews;
+
+import android.app.Application;
+
+import com.ashokvarma.gander.Gander;
+import com.ashokvarma.gander.imdb.GanderIMDB;
+
+public class TinNewsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: new code here.
+        Gander.setGanderStorage(GanderIMDB.getInstance());
+    }
+}
